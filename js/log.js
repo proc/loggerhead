5b0a89a2c78824602bcc47621f39771a2b0c00d8
Peter Tran <peter.tran@revolutionprep.com>
2012-01-04 18:58:44 -0800

    fs

--- js/log.js
+++ js/log.js
@@ -1,6 +1,14 @@
+var fs = require('fs');
+
 var Log = (function() {
   var lg = {};
+  var ws = fs.createWriteStream('./output.txt', {
+    flags: 'w',
+    encoding : null,
+    mode: 0666
+  });
   lg.write = function(str) {
+    ws.write(str + "\n");
     console.log(str);
   }
   return lg;
